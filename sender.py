6471dad2b1b286ef4414539e67f9fb9588f80f49
Artem LD31D <artem.12m21@gmail.com>
2020-09-04 16:05:30 -0400

    :sparkles: Adding a new feature - recursive receipt of files from the 'servises' directory and use of data from the file for requests

--- sender.py
+++ sender.py
@@ -1,23 +1,26 @@
+from os import listdir
+
 from fake_useragent import UserAgent
 from requests import post, exceptions
 
 
 def sender(phone: str):
-	with open('servises/uklon.txt', 'r') as file:
-		text = file.read().split('\n')
+	for file in listdir('servises'):
+		with open(f'servises/{file}', 'r') as file:
+			text = file.read().split('\n')
 
-		url = text[0]
+			url = text[0]
 
-		data = eval(text[1])
-		
-		for key in data:
-			if data[key] == "@PHONE@":
-				data[key] = phone
+			data = eval(text[1])
+			
+			for key in data:
+				if data[key] == "@PHONE@":
+					data[key] = phone
 
-		header = {'user-agent': UserAgent().random, **eval(text[2])}
+			header = {'user-agent': UserAgent().random, **eval(text[2])}
 
-		try:
-			post(url, data=data, headers=header)
+			try:
+				post(url, data=data, headers=header)
 
-		except exceptions.ConnectionError:
-			print("ConnectionError")
+			except exceptions.ConnectionError:
+				print("ConnectionError")
