d56b1fdc8374343981a6e1d3f875319a023a96ef
Artem LD31D <artem.12m21@gmail.com>
2020-09-05 04:50:30 -0400

    :hammer: Update code

--- sender.py
+++ sender.py
@@ -7,8 +7,8 @@ from requests import post, exceptions
 def sender(phone: str):
 	useragent = UserAgent().random
 
-	for filename in listdir('servises'):
-		with open(f'servises/{filename}', 'r') as file:
+	for filename in listdir('services'):
+		with open(f'services/{filename}', 'r') as file:
 			text = file.read().split('\n')
 
 			url = text[0]
@@ -16,8 +16,10 @@ def sender(phone: str):
 			data = eval(text[1])
 			
 			for key in data:
-				if data[key] == "@PHONE@":
-					data[key] = phone
+				if "@PHONE@" in data[key]:
+					data[key] = data[key].replace("@PHONE@", phone)
+
+					print(data[key])
 
 			header = {'user-agent': useragent, **eval(text[2])}
 
